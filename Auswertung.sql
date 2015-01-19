20209c2a49e3d72568eb4db85bf898b62eb265db
Stefan Waidele <Stefan@Waidele.info>
2015-01-20 00:31:50 +0100

    Bewertung Bluetooth

--- Auswertung.sql
+++ Auswertung.sql
@@ -8,9 +8,9 @@ select count(*)/119 from umfrage where einfachqr<=2 and einfachqr NOT LIKE "n/a"
 select count(*) from umfrage where smartphone="Ja" and einfachqr<3 and einfachqr NOT LIKE "n/a";
 
 
-select count(*) from umfrage where nutzungnfc=0.1;
-select count(*) from umfrage where nutzungnfc=0.0;
+select count(*) from umfrage where nutzungbt=0.1;
+select count(*) from umfrage where nutzungbt=0.0;
 
-select count(*) from umfrage where einfachnfc NOT LIKE "n/a";
-select count(*) from umfrage where einfachnfc<=2 and einfachnfc NOT LIKE "n/a";
+select count(*) from umfrage where einfachbt NOT LIKE "n/a";
+select count(*) from umfrage where einfachbt<=2 and einfachbt NOT LIKE "n/a";
 select count(*) from umfrage where smartphone="Ja" and einfachqr<3 and einfachnfc NOT LIKE "n/a";
