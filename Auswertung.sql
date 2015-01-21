d99d59f8524391af6f9338a88119d9b77d44bfb5
Stefan Waidele <Stefan@Waidele.info>
2015-01-21 17:12:31 +0100

    Bewertung BLE

--- Auswertung.sql
+++ Auswertung.sql
@@ -14,3 +14,11 @@ select count(*) from umfrage where nutzungbt=0.0;
 select count(*) from umfrage where einfachbt NOT LIKE "n/a";
 select count(*) from umfrage where einfachbt<=2 and einfachbt NOT LIKE "n/a";
 select count(*) from umfrage where smartphone="Ja" and einfachqr<3 and einfachnfc NOT LIKE "n/a";
+
+
+select count(*) from umfrage where nutzungble=0.1;
+select count(*) from umfrage where nutzungble=0.0;
+
+select count(*) from umfrage where einfachble NOT LIKE "n/a";
+select count(*) from umfrage where einfachble<=2 and einfachble NOT LIKE "n/a";
+select count(*) from umfrage where smartphone="Ja" and einfachble<3 and einfachble NOT LIKE "n/a";
