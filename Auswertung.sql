037358e3f83f1a96cca09ef44c34bfe01cb0ce7e
Stefan Waidele <Stefan@Waidele.info>
2015-01-26 16:41:54 +0100

    Nutzungstabellen Teil 1

--- Auswertung.sql
+++ Auswertung.sql
@@ -56,11 +56,10 @@ select count(*) from umfrage where nutzungqr>=1 AND nutzungbt>=1;
 select count(*) from umfrage where nutzungqr>=1 AND nutzungble>=1;
 select count(*) from umfrage where nutzungqr>=1 AND nutzungurl>=1;
 
-select nutzungqr, einfachnfc, einfachqr from umfrage where nutzungqr>=1 AND einfachnfc<=einfachqr AND einfachnfc NOT LIKE "n/a" AND einfachqr NOT LIKE "n/a";
-select nutzungqr, einfachbt, einfachqr from umfrage where nutzungqr>=1 AND einfachbt<=einfachqr AND einfachbt NOT LIKE "n/a" AND einfachqr NOT LIKE "n/a";
-select nutzungqr, einfachqr, einfachble from umfrage where nutzungqr>=1 AND einfachble<=einfachqr AND einfachble NOT LIKE "n/a" AND einfachqr NOT LIKE "n/a";
-select nutzungqr, einfachqr, einfachurl from umfrage where nutzungqr>=1 AND einfachurl<=einfachqr AND einfachurl NOT LIKE "n/a" AND einfachqr NOT LIKE "n/a";
-
+select count(*) from umfrage where nutzungqr>=1 AND einfachnfc<=einfachqr AND einfachnfc NOT LIKE "n/a" AND einfachqr NOT LIKE "n/a";
+select count(*) from umfrage where nutzungqr>=1 AND einfachbt<=einfachqr AND einfachbt NOT LIKE "n/a" AND einfachqr NOT LIKE "n/a";
+select count(*) from umfrage where nutzungqr>=1 AND einfachble<=einfachqr AND einfachble NOT LIKE "n/a" AND einfachqr NOT LIKE "n/a";
+select count(*) from umfrage where nutzungqr>=1 AND einfachurl<=einfachqr AND einfachurl NOT LIKE "n/a" AND einfachqr NOT LIKE "n/a";
 
 
 select count(*) from umfrage where nutzungnfc>=1;
@@ -86,3 +85,45 @@ select count(*) from umfrage where nutzungbt>=1 AND einfachnfc<=einfachbt AND ei
 select count(*) from umfrage where nutzungbt>=1 AND einfachble<=einfachbt AND einfachble NOT LIKE "n/a" AND einfachbt NOT LIKE "n/a";
 select count(*) from umfrage where nutzungbt>=1 AND einfachurl<=einfachbt AND einfachurl NOT LIKE "n/a" AND einfachbt NOT LIKE "n/a";
 
+
+select count(*) from umfrage where nutzungble>=1;
+select count(*) from umfrage where nutzungble>=1 AND nutzungqr>=1;
+select count(*) from umfrage where nutzungble>=1 AND nutzungnfc>=1;
+select count(*) from umfrage where nutzungble>=1 AND nutzungbt>=1;
+select count(*) from umfrage where nutzungble>=1 AND nutzungurl>=1;
+
+select count(*) from umfrage where nutzungble>=1 AND einfachqr<=einfachble AND einfachqr NOT LIKE "n/a" AND einfachble NOT LIKE "n/a";
+select count(*) from umfrage where nutzungble>=1 AND einfachnfc<=einfachble AND einfachbt NOT LIKE "n/a" AND einfachble NOT LIKE "n/a";
+select count(*) from umfrage where nutzungble>=1 AND einfachbt<=einfachble AND einfachbt NOT LIKE "n/a" AND einfachble NOT LIKE "n/a";
+select count(*) from umfrage where nutzungble>=1 AND einfachurl<=einfachble AND einfachurl NOT LIKE "n/a" AND einfachble NOT LIKE "n/a";
+
+
+select count(*) from umfrage where nutzungurl>=1;
+select count(*) from umfrage where nutzungurl>=1 AND nutzungqr>=1;
+select count(*) from umfrage where nutzungurl>=1 AND nutzungnfc>=1;
+select count(*) from umfrage where nutzungurl>=1 AND nutzungbt>=1;
+select count(*) from umfrage where nutzungurl>=1 AND nutzungble>=1;
+
+select count(*) from umfrage where nutzungurl>=1 AND einfachqr<=einfachurl AND einfachqr NOT LIKE "n/a" AND einfachurl NOT LIKE "n/a";
+select count(*) from umfrage where nutzungurl>=1 AND einfachnfc<=einfachurl AND einfachbt NOT LIKE "n/a" AND einfachurl NOT LIKE "n/a";
+select count(*) from umfrage where nutzungurl>=1 AND einfachbt<=einfachurl AND einfachbt NOT LIKE "n/a" AND einfachurl NOT LIKE "n/a";
+select count(*) from umfrage where nutzungurl>=1 AND einfachurl<=einfachurl AND einfachurl NOT LIKE "n/a" AND einfachurl NOT LIKE "n/a";
+
+
+select count(*) from umfrage;
+select count(*) from umfrage where geschlecht = "m" ;
+select count(*) from umfrage where geschlecht = "w" ;
+
+select count(*) from umfrage where smartphone = "Ja" ;
+select count(*) from umfrage where smartphone = "Nein" ;
+
+select count(os) from umfrage where os = "Android" ;
+select count(os) from umfrage where os = "iOS" ;
+select count(os) from umfrage where os = "Win" ;
+
+select geschlecht, count(geschlecht), count(geschlecht)/174*100 from umfrage group by geschlecht ;
+select age, count(age), count(age)/174*100 from umfrage group by age ;
+
+select smartphone, count(smartphone), count(smartphone)/174*100 from umfrage group by smartphone ;
+select count(os) from umfrage where  os="Android" or os="iOS" or os="Win";
+select os, count(os), count(os)/166*100 from umfrage group by os ;
